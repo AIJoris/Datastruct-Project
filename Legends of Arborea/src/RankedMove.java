e1ddea7ea7ce6007f9c04ae92d9a845d0bc5a441
Joris <jsbaan@gmail.com>
2015-03-15 16:18:17 +0100

    k

---
+++ Legends of Arborea/src/RankedMove.java
@@ -0,0 +1,12 @@
+/*
+ * This class determines the move and it rank
+ */
+public class RankedMove {
+	double rank;
+	Tile move;
+	
+	public RankedMove(Tile move1, double rank1){
+		move = move1;
+		rank = rank1;
+	}
+}
